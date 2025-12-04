98ea3f1ebc680fe71584ddf4e7245c860903cb53
ahnafmottaki <ahnafmottaki2022@gmail.com>
2025-12-04 12:17:51 +0600

    migrate to tailwindcss

--- client/src/components/Header/Header.tsx
+++ client/src/components/Header/Header.tsx
@@ -1,27 +1,26 @@
-import styles from "./header.module.css";
 import logo from "../../assets/shared/desktop/logo.svg";
 import { Menu, ShoppingCart } from "lucide-react";
-import MobileNav from "../MobileNav/MobileNav";
-import { useState } from "react";
+import { NAVIGATION_LINKS } from "../../constants/navigationLinks";
 const Header = () => {
-  const [isMenuOpen, setIsMenuOpen] = useState(false);
-  const handleIsMenuOpen = () => {
-    setIsMenuOpen(!isMenuOpen);
-  };
   return (
-    <header className={styles.wrapper}>
-      <MobileNav isOpen={isMenuOpen} />
-      <div className={`container ${styles.header}`}>
-        <div className={styles.logo}>
-          <Menu className="icon" onClick={handleIsMenuOpen} />
+    <header className="bg-black relative max-[1150px]:px-10 max-md:px-0">
+      <div
+        className={`container pt-8 pb-9 flex justify-between items-center border-b border-b-header-bottom`}
+      >
+        <div className={"flex items-center gap-x-10.5"}>
+          <Menu className="lg:hidden inline text-white" />
           <img src={logo} alt="Audiophile logo" />
         </div>
-        <nav className={styles.nav}>
-          <ul>
-            <li>home</li>
-            <li>headphones</li>
-            <li>speakers</li>
-            <li>earphones</li>
+        <nav className="hidden lg:block ">
+          <ul className="flex items-center gap-x-8.5 ">
+            {NAVIGATION_LINKS.map((li) => (
+              <li
+                key={li.href}
+                className="font-bold text-[0.813rem] leading-6 tracking-[2px] uppercase cursor-pointer transition-colors duration-200 text-white hover:text-primary"
+              >
+                <a href={li.href}>{li.text}</a>
+              </li>
+            ))}
           </ul>
         </nav>
         <div>
---
+++ client/src/constants/navigationLinks.ts
@@ -0,0 +1,8 @@
+const NAVIGATION_LINKS = [
+  { text: "home", href: "home" },
+  { text: "headphones", href: "headphones" },
+  { text: "speakers", href: "speakers" },
+  { text: "earphones", href: "earphones" },
+] as const;
+
+export { NAVIGATION_LINKS };
